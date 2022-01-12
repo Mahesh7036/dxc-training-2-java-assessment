4f8e74fef123d854dd36c2edaaaa99b9fb78246d
Mahesh7036 <96856448+Mahesh7036@users.noreply.github.com>
2022-01-12 15:59:53 +0530

    Assign1

---
+++ Assignment1/AssessmentTest.java
@@ -0,0 +1,14 @@
+package assesment1;
+
+public class AssessmentTest {
+
+	public static void main(String[] args) {
+
+MonsterTruck m = new MonsterTruck();
+m.m1();
+m.m2();
+System.out.println(m.toString());
+
+	}
+
+}
